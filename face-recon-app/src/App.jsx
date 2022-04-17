fb8860aa1aa128b469a542f104a742a4b2a844e6
Paulo Filho <paulomacieltorresfilho@gmail.com>
2022-04-17 10:17:30 -0300

    componente App estilizado

--- face-recon-app/src/App.jsx
+++ face-recon-app/src/App.jsx
@@ -1,8 +1,62 @@
 import React, { useState, useEffect } from "react";
+import { 
+  Button,
+  Paper, 
+  Typography,
+  Stack 
+} from '@mui/material';
+import { styled } from '@mui/material/styles';
 import './App.css';
 
+const SPaper = styled(Paper)({
+  backgroundColor: 'white',
+  height: '600px',
+  width: '80%',
+  maxWidth: '1000px',
+  margin: 'auto',
+  marginTop: '100px',
+
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+})
+
+const Title = styled(Typography)({
+  marginTop: '8px',
+  color: '#2c6fd1',
+  borderBottom: '1px solid black',
+  width: '60%',
+  textAlign: 'center'
+})
+
+const Container = styled('div')({
+  height: '100%',
+  width: '100%',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+})
+
+const SStack = styled(Stack)({
+  textAlign: 'center'
+})
+
+const Input = styled('input')({
+  display: 'none',
+})
+
+const ImgPreview = styled('img')({
+  maxWidth: '256px',
+})
+
+const SButton = styled(Button)({
+  marginTop: '10px',
+  marginBottom: '10px'
+})
+
 const App = () => {
-  const [selectedFile, setSelectedFile]= useState();
+  const [selectedFile, setSelectedFile] = useState();
   const [preview, setPreview] = useState()
 
   useEffect(() => {
@@ -35,18 +89,25 @@ const App = () => {
   }
 
   return (
-    <div className="App">
-      <h1>AM Project Name</h1>
-      {selectedFile ? 
-      <img className="previewImg" alt="Preview" src={preview} /> : 
-      <div></div>
-      }
-      <input type="file" accept=".jpg, .jpeg, .png" onChange={fileSelectedHandler} />
-      {/* State when clearing not updating correct find out why */}
-      {/* <button onClick={clearSelectedFile}>Clear Image</button> */}
-      <button onClick={predictFelling}>Prever Sentimento</button>
-    </div>
+    <SPaper elevation={6} className="App">
+      <Title variant="h4">Expression Recognizer</Title>
+      <Container>
+        <SStack spacing={0.5}>
+          {selectedFile &&
+          <ImgPreview alt="Preview" src={preview} />
+          }
+          <label>
+            <Input type="file" accept=".jpg, .jpeg, .png" onChange={fileSelectedHandler} />
+            <SButton id="upload-button" variant="outlined" size="small" component="span">
+              Escolher Arquivo
+            </SButton>
+          </label>
+          {/* State when clearing not updating correct find out why */}
+          {/* <button onClick={clearSelectedFile}>Clear Image</button> */}
+          <Button variant="contained" onClick={predictFelling}>Prever Sentimento</Button>
+        </SStack>
+      </Container>
+    </SPaper>
   );
 }
-
 export default App;
